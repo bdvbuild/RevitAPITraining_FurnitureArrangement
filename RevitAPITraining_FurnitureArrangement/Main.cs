68c5074bee706e960ce68db825f1f0b6e50e7340
bdvbu <bdvbu@DBARANOV>
2023-02-08 12:42:30 +0400

    Добавьте файлы проекта.

---
+++ RevitAPITraining_FurnitureArrangement/Main.cs
@@ -0,0 +1,22 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace RevitAPITraining_FurnitureArrangement
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Сообщение", "Тест");
+            return Result.Succeeded;
+        }
+    }
+}
